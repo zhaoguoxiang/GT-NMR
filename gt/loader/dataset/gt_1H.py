88f93724dda2e943f38559010675a82231ed379a
Guoxiang Zhao <mrzhaoguoxiang@outlook.com>
2025-04-29 11:21:24 +0800

    tmp error

--- gt/loader/dataset/gt_1H.py
+++ gt/loader/dataset/gt_1H.py
@@ -71,13 +71,41 @@ class gt1H(InMemoryDataset):
             data = Data()
 
             total_num_nodes = mol.GetNumAtoms()
-            label_tensor = torch.zeros(total_num_nodes, 2)
+            # 增加一个维度，计算该原子上所含H的数目
+            label_tensor = torch.zeros(total_num_nodes, 3)
             for data_dict in label:
                 for key, value in data_dict.items():
                     label_tensor[key][1] = value
 
             for atom in range(mol.GetNumAtoms()):
                 label_tensor[atom][0] = mol.GetAtomWithIdx(atom).GetAtomicNum()
+            # todo 记录所含H的数目
+            for atom in range(mol.GetNumAtoms()):
+                if mol.GetAtomWithIdx(atom).GetAtomicNum() == 1:
+                    label_tensor[atom][2] = 0
+                else:
+                    h_count = sum(1 for nei in mol.GetAtomWithIdx(atom).GetNeighbors() if nei.GetSymbol() == 'H')
+                    label_tensor[atom][2] = h_count
+
+            label_tensor_after = assign_labels_to_connected_atoms(mol, label_tensor)
+            mol_withoutHs = Chem.RemoveHs(mol)
+
+            mask_hs = mask_H(label_tensor_after)
+            label_tensor_after = label_tensor_after[mask_hs]
+
+            graph = mol2graph(mol_withoutHs)
+            mask_other  = mask_others(label_tensor_after)
+            infer_mask = infer_mask_fun(label_tensor_after)
+
+            y = label_tensor_after[:, 1]
+            x = torch.from_numpy(graph['node_feat']).to(torch.long)
+            edge_index = torch.from_numpy(graph['edge_index']).to(torch.long)
+            edge_attr = torch.from_numpy(graph['edge_feat']).to(torch.long)
+
+            data.__num_nodes__ = int(graph['num_nodes'])
+            data.x = x
+            data.edge_index = edge_index
+            data.edge_attr = edge_attr
 
             label_tensor_after = assign_labels_to_connected_atoms(mol, label_tensor)
             mol_withoutHs = Chem.RemoveHs(mol)
@@ -114,9 +142,12 @@ class gt1H(InMemoryDataset):
 def assign_labels_to_connected_atoms(mol, atom_labels_tensor):
     result_tensor = torch.zeros_like(atom_labels_tensor)
     result_tensor[:, 0] = atom_labels_tensor[:, 0]
-    valid_indices = [i for i, (idx, label) in enumerate(atom_labels_tensor) if idx == 1 and label > 0]
+    # valid_indices 是idx（原子序数）1，H原子，且化学位移大于０
+    valid_indices = [i for i, (idx, label, h_count) in enumerate(atom_labels_tensor) if idx == 1 and label > 0]
     # print(valid_indices)
+    # 同一个C原子上的H有不同的位移，因此需要对同一个C原子上的H进行平均化学位移　labels_sum = {}
     labels_sum = {}
+    # 统计每个原子上H和数量
     count = {}
 
     for valid_idx in valid_indices:
@@ -145,7 +176,7 @@ def assign_labels_to_connected_atoms(mol, atom_labels_tensor):
 
     for valid_idx in valid_indices:
         result_tensor[valid_idx, 1] = 0
-
+    # 返回count为添加mask做准备
     return result_tensor
 
 def mask_H(x: torch.Tensor):
@@ -160,8 +191,10 @@ def mask_H(x: torch.Tensor):
 def mask_others(x: torch.Tensor):
     mask = torch.zeros(x.shape[0], dtype=torch.bool)
     for i in range(x.shape[0]):
-        if x[i][0] == 6.0000 and x[i][1] != 0:
-            mask[i] = True  # C with label is True
+        #! 原始的判断有问题，应该是有H原子相连的mask为true
+        if x[i][2] > 0 and x[i][1] != 1.0000:
+        # if x[i][1] ==6.0000 and x[i][1] != 1.0000:
+            mask[i] = True  # atoms with H are True
         else:
             mask[i] = False
     return mask
@@ -170,7 +203,8 @@ def mask_others(x: torch.Tensor):
 def infer_mask_fun(x: torch.Tensor):
     mask = torch.zeros(x.shape[0], dtype=torch.bool)
     for i in range(x.shape[0]):
-        if x[i][0] == 6.0000:
+        if x[i][2] > 0 and x[i][1] != 1.0000:
+        # if x[i][0] == 6.0000:
             mask[i] = True  # C is True
         else:
             mask[i] = False
